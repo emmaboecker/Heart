bc811b1490f02ba70475ba6c7ea1452d1429dd13
Marcel Böcker <57072067+StckOverflw@users.noreply.github.com>
2021-11-09 18:32:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# KSpigot Gradle Kotlin Template
+This is a template for creating a [Paper Plugin](https://papermc.io//) with [KSpigot](https://github.com/jakobkmar/kspigot), [Gradle Kotlin DSL](https://docs.gradle.org/current/userguide/kotlin_dsl.html) and the [Kotlin](https://kotlinlang.org) programming language.
+
+## How to use
+1. Click **Use this template** at the top.
+2. Clone your version.
+3. Open the Folder in IntelliJ 
+4. Rename group in **build.gradle.kts**
+5. Rename project name in **settings.gradle.kts**
+6. Edit your **plugin.yml** to your needs
+
+## License
+This template is licensed under the [MIT License](https://choosealicense.com/licenses/mit/)
+
+Copyright (c) 2021 StckOverflw
---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "org.example"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.codemc.io/repository/maven-snapshots/")
+}
+
+dependencies {
+    // PaperMC Dependency
+    compileOnly("io.papermc.paper:paper-api:1.17-R0.1-SNAPSHOT") // Only used on compile time because we have a PaperMC Server so we don't need it in the final jar
+
+    // KSpigot dependency
+    implementation("net.axay:kspigot:1.17.4")
+
+    // You can add Dependencies here
+    // For Example:
+    // implementation("io.ktor", "ktor-client", "1.4.0") // This would be shaded into the final jar
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "16"
+        }
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+}
